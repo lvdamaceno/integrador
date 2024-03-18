c7a9c0c1923c92ec5115db2b5e86086156809be7
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-03-17 23:09:35 -0300

    update details

--- src/app/vtex/page.tsx
+++ src/app/vtex/page.tsx
@@ -44,47 +44,65 @@ export default function Page() {
           </button>
         </div>
       </div>
-      <div className="flex flex-col gap-4 sm:flex-row">
-        <div className="flex flex-col gap-2 items-center border border-solid rounded-xl p-4 w-60">
-          <label htmlFor="enviarInformacaoProduto">Atualizar produto</label>
-          <input
-            className="border border-solid rounded-lg text-center w-full mb-2"
-            type="number"
-            name="enviarInformacaoProduto"
-            placeholder="Ex: 123456"
-            onChange={(e) => {
-              setValue(e.currentTarget.value);
-            }}
-          />
-          <button
-            onClick={(event) => {
-              enviarInformacaoProduto(value, atualizaEndpoint);
-            }}
-            className="p-1 border border-solid bg-zinc-100 rounded-lg w-full hover:bg-zinc-300"
-          >
-            Atualizar
-          </button>
+      <div className="flex flex-col gap-4 items-center">
+        <div className="w-3/4 mb-6">
+          <h2 className="underline font-bold text-1xl">
+            Antes de atualizar/cadastrar produtos certifique-se que:
+          </h2>
+          <ul className="list-decimal">
+            <li>
+              O grupo de produtos está informado no campo <strong>Menu</strong>{' '}
+              na aba <strong>E-commerce</strong> do cadastro de produtos no
+              Sankhya.
+            </li>
+            <li>
+              O grupo dos produtos está na faixa <strong>5.00.00.00</strong>
+            </li>
+          </ul>
         </div>
 
-        <div className="flex flex-col gap-2 items-center border border-solid rounded-xl p-4  w-60">
-          <label htmlFor="enviarInformacaoProduto">Cadastrar produto</label>
-          <input
-            className="border border-solid rounded-lg text-center w-full mb-2"
-            type="number"
-            name="cadastraProduto"
-            placeholder="Ex: 123456"
-            onChange={(e) => {
-              setValue(e.currentTarget.value);
-            }}
-          />
-          <button
-            onClick={(event) => {
-              enviarInformacaoProduto(value, cadastraEnpoint);
-            }}
-            className="p-1 border border-solid bg-zinc-100 rounded-lg w-full hover:bg-zinc-300"
-          >
-            Cadastrar
-          </button>
+        <div className="flex flex-col gap-4 sm:flex-row">
+          <div className="flex flex-col gap-2 items-center border border-solid rounded-xl p-4 w-60">
+            <label htmlFor="enviarInformacaoProduto">Atualizar produto</label>
+            <input
+              className="border border-solid rounded-lg text-center w-full mb-2 p-2"
+              type="number"
+              name="enviarInformacaoProduto"
+              placeholder="Ex: 123456"
+              onChange={(e) => {
+                setValue(e.currentTarget.value);
+              }}
+            />
+            <button
+              onClick={(event) => {
+                enviarInformacaoProduto(value, atualizaEndpoint);
+              }}
+              className="p-1 border border-solid bg-zinc-100 rounded-lg w-full hover:bg-zinc-300"
+            >
+              Atualizar
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-2 items-center border border-solid rounded-xl p-4 w-60">
+            <label htmlFor="enviarInformacaoProduto">Cadastrar produto</label>
+            <input
+              className="border border-solid rounded-lg text-center w-full mb-2 p-2"
+              type="number"
+              name="cadastraProduto"
+              placeholder="Ex: 123456"
+              onChange={(e) => {
+                setValue(e.currentTarget.value);
+              }}
+            />
+            <button
+              onClick={(event) => {
+                enviarInformacaoProduto(value, cadastraEnpoint);
+              }}
+              className="p-1 border border-solid bg-zinc-100 rounded-lg w-full hover:bg-zinc-300"
+            >
+              Cadastrar
+            </button>
+          </div>
         </div>
       </div>
     </div>
