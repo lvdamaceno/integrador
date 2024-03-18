a3b2b9bc0af1efa8787afc38f99c69b833e0da4d
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-03-17 22:38:52 -0300

    init commit

---
+++ src/app/vtex/page.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { Money, Receipt, Warehouse } from '@phosphor-icons/react';
+import { useState } from 'react';
+
+const baseUrl = process.env.NEXT_PUBLIC_URL_BASE;
+const atualizaEndpoint = process.env.NEXT_PUBLIC_ENDPOINT_ATUALIZAR;
+const cadastraEnpoint = process.env.NEXT_PUBLIC_ENDPOINT_CADASTRAR;
+
+async function enviarInformacaoProduto(codprod: string, endpoint: any) {
+  try {
+    const response = await fetch(`${baseUrl}${endpoint}${codprod}`);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.error('Erro ao buscar dados:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+  }
+}
+
+export default function Page() {
+  const [value, setValue] = useState('');
+  return (
+    <div className="flex flex-col gap-10 items-center mt-10">
+      <h1 className="underline font-bold text-2xl">
+        Integrador Sankhya \ VTEX
+      </h1>
+      <div className="">
+        <div className="flex flex-col gap-4 sm:flex-row">
+          <button className="flex p-2 gap-2 bg-zinc-100 hover:bg-zinc-300 rounded-xl w-60 justify-center">
+            <Money size={24} /> Sincronizar preços
+          </button>
+          <button className="flex p-2 gap-2 bg-zinc-100 hover:bg-zinc-300 rounded-xl w-60 justify-center">
+            <Warehouse size={24} />
+            Sincronizar estoques
+          </button>
+          <button className="flex p-2 gap-2 bg-zinc-100 hover:bg-zinc-300 rounded-xl w-60 justify-center">
+            <Receipt size={24} />
+            Sincronizar pedidos
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <div className="flex flex-col gap-2 items-center border border-solid rounded-xl p-4 w-60">
+          <label htmlFor="enviarInformacaoProduto">Atualizar produto</label>
+          <input
+            className="border border-solid rounded-lg text-center w-full mb-2"
+            type="number"
+            name="enviarInformacaoProduto"
+            placeholder="Ex: 123456"
+            onChange={(e) => {
+              setValue(e.currentTarget.value);
+            }}
+          />
+          <button
+            onClick={(event) => {
+              enviarInformacaoProduto(value, atualizaEndpoint);
+            }}
+            className="p-1 border border-solid bg-zinc-100 rounded-lg w-full hover:bg-zinc-300"
+          >
+            Atualizar
+          </button>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center border border-solid rounded-xl p-4  w-60">
+          <label htmlFor="enviarInformacaoProduto">Cadastrar produto</label>
+          <input
+            className="border border-solid rounded-lg text-center w-full mb-2"
+            type="number"
+            name="cadastraProduto"
+            placeholder="Ex: 123456"
+            onChange={(e) => {
+              setValue(e.currentTarget.value);
+            }}
+          />
+          <button
+            onClick={(event) => {
+              enviarInformacaoProduto(value, cadastraEnpoint);
+            }}
+            className="p-1 border border-solid bg-zinc-100 rounded-lg w-full hover:bg-zinc-300"
+          >
+            Cadastrar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
